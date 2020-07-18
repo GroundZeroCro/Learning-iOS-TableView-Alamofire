dc594147734a2ac68212f26d453a7e5f8fde13b4
groundzero <mario.dujic@undabot.com>
2020-07-19 01:07:44 +0200

    status bar changed to light

--- Flowers/Common/MyNavigationBar.swift
+++ Flowers/Common/MyNavigationBar.swift
@@ -1,8 +1,3 @@
-//
-// Created by ground zero on 18/07/2020.
-// Copyright (c) 2020 TrotterIndependent. All rights reserved.
-//
-
 import Foundation
 import UIKit
 
@@ -11,12 +6,13 @@ class MyNavigationBar: UINavigationBar {
     private var title: String = ""
 
     init(title: String) {
-        super.init(frame: CGRect(x: 0, y: 0, width: DeviceUtils.screenWidth, height: 90))
+        super.init(frame: CGRect(x: 0, y: 0, width: DeviceUtils.screenWidth, height: 100))
         self.title = title
 
         backgroundColor = Colors.tile
         largeTitleTextAttributes = [.foregroundColor: UIColor.white]
         prefersLargeTitles = true
+        layoutMargins = UIEdgeInsets(top: 0, left: Dimensions.normalPadding, bottom: 0, right: -25)
 
         let navigationItem = UINavigationItem(title: self.title)
         setItems([navigationItem], animated: false)
